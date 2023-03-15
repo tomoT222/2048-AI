16f83771aa0d19aa92991fa5ffecaaf990ddebbe
tomoT222 <65435187+tomoT222@users.noreply.github.com>
2023-03-15 12:47:47 +0900

    Create f(x).py

---
+++ practiceOptuna/f(x).py
@@ -0,0 +1,11 @@
+import optuna
+
+def f(x):
+    return x**4 - 4 * x ** 3 - 36 * x ** 2
+
+def objective(trial):
+    x = trial.suggest_uniform('x', -10, 10)
+    return f(x)
+
+study = optuna.create_study()
+study.optimize(objective, n_trials=10)
